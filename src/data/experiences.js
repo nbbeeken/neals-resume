95d3389dcc538c3ba0c211028ef304f23cfe7350
Neal Beeken <nbbeeken@gmail.com>
2018-09-17 11:01:37 -0400

    Add GPA, Change Date Font, Add tags

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -1,31 +1,35 @@
 exports.experiences = [
     {
         name: `Graduate Teaching Assistant`,
+        company: "Stony Brook University",
         icon: "nf-fa-graduation_cap",
-        date: "Aug 17 - May 18",
+        date: "Aug 2017-May 2018",
         description: `
-            Created original homeworks
-            relating to UDP music streaming and P2P protocols
-        `
+            Assisted in material creation for coursework.
+            Trained and led a team of TAs in homework design.
+            Graded exams and coding assignments.
+        `.trim()
     },
     {
-        name: `SBU Smart Transit Android Developer`,
+        name: `Android Developer`,
+        company: "Smart Transit at CEWIT",
         icon: "nf-dev-android",
-        date: "Aug 17 - May 18",
+        date: "Aug 2017-May 2018",
         description: `
-            Repaired and maintained existing campus bus/shuttle tracking Java Android app
-            Ground-up rework of Android app in Kotlin, reducing app size and improving performance
-        `
+            Repaired and maintained existing campus bus and shuttle tracking Android app in Java.
+            Subsequently performed a ground-up rework of the Android app in Kotlin,
+            reducing app size and improving performance while upgrading the UI/UX.
+        `.trim()
     },
     {
         name: `Undergraduate Teaching Assistant`,
+        company: "Stony Brook University",
         icon: "nf-fa-graduation_cap",
-        date: "Jan 16 - May 17",
+        date: "Jan 2016-May 2017",
         description: `
-            Participated in assignment development for system-level C
-            programming course. Created Python scripts  to compile
-            and run students submissions for grading. Led and trained
-            a team of TAs in homework design
-        `
+            Participated in assignment development for system-level C programming course.
+            Created Python scripts to compile and run students submissions for grading.
+            Held office hours teaching students and providing help debugging through discussion.
+        `.trim()
     },
 ];
--- src/data/projects.js
+++ src/data/projects.js
@@ -11,71 +11,79 @@ exports.projects = [
     {
         name: `"Dashmips" — MIPS Assembly simulator`,
         icon: "nf-seti-python",
-        date: "Jun 18 - Current",
+        date: "Jun 2018-Current",
+        technologies: ["python", "regex", "mips"],
         description: `
-            CLI python package to simulate a MIPS program by interpreting asm text
-            Future plans for vscode extention to enable debugging and visualization
-        `
+            CLI python package to simulate a MIPS program by interpreting asm text.
+            Future plans for vscode extention to enable debugging and visualization.
+        `.trim()
     },
     {
         name: `"DASH" — Automated Interactive Course Orchestration`,
-        icon: "nf-seti-python",
-        date: "Aug 17 - Apr 18",
+        icon: "nf-dev-git",
+        date: "Aug 2017-Apr 2018",
+        technologies: ["python", "gitlab", "git"],
         description: `
             An interactive tool to assist in setting up a private gitlab
             instance to be used for source submission along with assignment
-            distribution
-        `
+            distribution.
+        `.trim()
     },
     {
         name: `"Moon OS" — Operating System`,
         icon: "nf-custom-c",
-        date: "Aug 17 - Dec 17",
+        date: "Aug 2017-Dec 2017",
+        technologies: ["c", "x86", "shell", "memory allocator", "ahci", "qemu"],
         description: `
             An original operating system in C: Context switching,
             Virtual Memory management, COW fork, Userspace processes,
-            Runs on real hardware (and qemu)
-        `
+            Runs on real hardware (and qemu).
+        `.trim()
     },
     {
-        name: `"Glitter" — Full Stack Social Networking Web Application`,
-        icon: "nf-mdi-nodejs",
-        date: "Jan 17 - May 17",
+        name: `"Glitter" — Full Stack Social Networking Web App`,
+        icon: "nf-mdi-nodejs", //"nf-dev-javascript_badge", //"nf-seti-javascript",
+        date: "Jan 2017-May 2017",
+        technologies: ["nodejs", "ansible", "mongodb", "IAAS"],
         description: `
-            A Rails and NodeJS Web Server:
-            utilizing unicorn workers / pm2, a clustered MongoDB database,
-            Ansible for continuous deployment
-        `
+            A NodeJS Web Server utilizing IAAS deployment:
+            nodejs multi-process clustering,
+            clustered MongoDB database,
+            Ansible for continuous deployment.
+        `.trim()
     },
     {
         name: `"Wolfie Script" — Original Scripting Language`,
         icon: "nf-seti-python",
-        date: "May 16 - Aug 16",
+        date: "May 2016-Aug 2016",
+        technologies: ["python", "ply", "AST"],
         description: `
             Implemented Abstract Syntax Tree in Python PLY library:
             duck type variables, shallow scoping, if/else statements,
-            while loops
-        `
+            while loops.
+        `.trim()
     },
     {
         name: `"Atomic Comics" — Comic Creation tool`,
         icon: "nf-dev-angular",
-        date: "Jan 16 - May 16",
+        date: "Jan 2016-May 2016",
+        technologies: ["angularjs", "java", "HTML5 Canvas", "PAAS"],
         description: `
-            Web-app in AngularJS and Java Google App Engine: Saved user
+            Web-app in AngularJS and Java utilizing PAAS deployment: Saved user
             content generated on HTML5 Canvas and Published user content
-            to landing page
-        `
+            to landing page.
+        `.trim()
     },
     {
         name: `"WolfieBall" — Java Fantasy Baseball`,
         icon: "nf-fae-java",
-        date: "Jan 15 - May 15",
+        date: "Jan 2015-May 2015",
+        technologies: ["java", "javafx", "JSON", "multithreading"],
         description: `
             Java Desktop App: Loaded MLB JSON data,
             assisted in constructing fantasy teams,
             used multithreading to auto populate teams,
-            helped professor win his MLB Fantasy League (Apr 17)
-        `
+            helped professor win his MLB Fantasy League (Apr 17).
+        `.trim()
     },
 ];
