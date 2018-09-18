1184887d74cebef5e2672d577832fc7c281e1e4e
Neal Beeken <nbbeeken@gmail.com>
2018-09-18 11:55:25 -0400

    Add Helpful Changes

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -3,32 +3,34 @@ exports.experiences = [
         name: `Graduate Teaching Assistant`,
         company: "Stony Brook University",
         icon: "nf-fa-graduation_cap",
-        date: "Aug '17-May '18",
+        date: "Aug '17 - May '18",
         description: `
-            Assisted in material creation for coursework.
-            Trained and led a team of TAs in homework design.
-            Graded exams and coding assignments.
+            Assisted in creating coding assignment documentation
+            and regression unit tests for students.
+            Led a team of Teaching Assistants by supervising the development of
+            homework assignments.
+            Wrote automated grading tools for coding assignments.
         `.trim()
     },
     {
         name: `Android Developer`,
         company: "Smart Transit at CEWIT",
         icon: "nf-dev-android",
-        date: "Aug '17-May '18",
+        date: "Aug '17 - May '18",
         description: `
             Repaired and maintained existing campus bus and shuttle tracking Android app in Java.
-            Subsequently performed a ground up rework of the Android app in Kotlin,
-            reducing app size and improving performance while upgrading the UI/UX.
+            Recreated the Android app in Kotlin, reducing app size by 50% and
+            improving network performance and UI responsiveness.
         `.trim()
     },
     {
         name: `Undergraduate Teaching Assistant`,
         company: "Stony Brook University",
         icon: "nf-fa-graduation_cap",
-        date: "Jan '16-May '17",
+        date: "Jan '16 - May '17",
         description: `
             Participated in assignment development for system-level C programming course.
-            Created robust Python scripts to compile and run student submissions for grading.
+            Created robust Python scripts to automate testing of student submissions for grading.
             Held office hours teaching students and providing help with debugging through discussion.
         `.trim()
     },
--- src/data/projects.js
+++ src/data/projects.js
@@ -9,87 +9,93 @@ interface IProject {
 
 exports.projects = [
     {
-        name: `"Dashmips" — MIPS Assembly simulator`,
+        name: `"Dashmips" — MIPS Assembly Simulator`,
         icon: "nf-seti-python",
-        date: "Jun '18-Current",
+        date: "Jun '18 - Current",
         technologies: ["python", "regex", "mips"],
         description: `
-            CLI python package to simulate a MIPS program by interpreting assembly text.
-            Future plans for vscode extension to enable debugging and visualization.
+            Command line python package to simulate a
+            MIPS program by interpreting assembly text.
+            Future plans for an editor extension that
+            enables MIPS debugging and visualization.
         `.trim()
     },
     {
         name: `"DASH" — Automated Interactive Course Orchestration`,
         icon: "nf-dev-git",
-        date: "Aug '17-Apr '18",
+        date: "Aug '17 - Apr '18",
         technologies: ["python", "gitlab", "git"],
         description: `
-            An interactive tool to assist in setting up a private gitlab
-            instance to be used for source submission along with assignment
+            An interactive tool to assist deploying a private gitlab
+            instance for the purpose of source submission along with assignment
             distribution.
         `.trim()
     },
     {
         name: `"Moon OS" — Operating System`,
         icon: "nf-custom-c",
-        date: "Aug '17-Dec '17",
+        date: "Aug '17 - Dec '17",
         technologies: ["c", "x86_64", "shell", "memory allocator", "ahci", "qemu"],
         description: `
             A 64-bit operating system in C targeting the x86_64 platform.
             Features include:
             context switching,
-            harddrive AHCI driver
+            AHCI driver
             virtual memory management,
-            COW fork,
+            copy-on-write fork,
             and userspace processes.
-            Tested on qemu and real hardware.
+            Tested on real hardware.
         `.trim()
     },
     {
         name: `"Glitter" — Full Stack Social Networking Web App`,
         icon: "nf-mdi-nodejs", //"nf-dev-javascript_badge", //"nf-seti-javascript",
-        date: "Jan '17-May '17",
+        date: "Jan '17 - May '17",
         technologies: ["nodejs", "ansible", "mongodb", "IAAS"],
         description: `
-            A NodeJS Web Server utilizing IAAS deployment:
-            nodejs multi-process clustering,
-            clustered MongoDB database,
-            Ansible for continuous deployment.
+            A RESTful social networking service written in NodeJS.
+            The service was deployed to a private cloud using Ansible.
+            and scaled to respond to millions of client requests
+            using multi-process clustering and a sharded MongoDB database.
         `.trim()
     },
     {
         name: `"Wolfie Script" — Original Scripting Language`,
         icon: "nf-seti-python",
-        date: "May '16-Aug '16",
+        date: "May '16 - Aug '16",
         technologies: ["python", "ply", "AST"],
         description: `
-            Implemented Abstract Syntax Tree in Python PLY library:
+            An original scripting language implemented
+            using the Python PLY (yacc/lex) library with support for:
             duck type variables, shallow scoping, if/else statements,
             while loops.
         `.trim()
     },
     {
-        name: `"Atomic Comics" — Comic Creation tool`,
+        name: `"Atomic Comics" — Comic Creation Web App`,
         icon: "nf-dev-angular",
-        date: "Jan '16-May '16",
+        date: "Jan '16 - May '16",
         technologies: ["angularjs", "java", "HTML5 Canvas", "PAAS"],
         description: `
-            Web app in AngularJS and Java utilizing PAAS deployment:
-            Saved user generated content from HTML5 Canvas.
-            Published canvas contents to landing page and
-            enabled users to modify saved work.
+            AngularJS and Java web app deployed to PAAS environment:
+            Features included
+            saving user-generated content from HTML5 Canvas,
+            publishing canvas contents to the landing page, and
+            enabling users to modify saved work.
         `.trim()
     },
     {
-        name: `"WolfieBall" — Java Fantasy Baseball`,
+        name: `"WolfieBall" — Fantasy Baseball Desktop Application`,
         icon: "nf-fae-java",
-        date: "Jan '15-May '15",
-        technologies: ["java", "javafx", "JSON", "multithreading"],
+        date: "Jan '15 - May '15",
+        technologies: ["java", "JSON", "multithreading"],
         description: `
-            Java Desktop App: Loaded MLB JSON data,
-            assisted in constructing fantasy teams,
-            used multithreading to auto populate teams,
-            helped professor win his MLB Fantasy League (Apr '17).
+            A desktop application written in Java and JavaFX
+            that allowed users to load saved MLB JSON data,
+            construct fantasy baseball teams manually and
+            auto-generate teams in a separate thread.
+            Two years later helped professor win his MLB Fantasy League
+            (Apr '17).
         `.trim()
     },
 ];
